6501cc8a4285d3d2e077401169b4c8246bab2b48
Matthew Hiebl <87024986+matthiebl@users.noreply.github.com>
2023-05-11 11:00:53 +1000

    Undo basename

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { UpgradesPage } from './pages/UpgradeCostCalc'
 
 const App = () => (
-    <BrowserRouter basename='factory-idle-calculator'>
+    <BrowserRouter>
         <Routes>
             <Route path='/' element={<UpgradesPage />} />
         </Routes>
