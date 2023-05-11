3ebf6b539c0bbc1c7d27c49b1384e950b908b4ad
Matthew Hiebl <87024986+matthiebl@users.noreply.github.com>
2023-05-11 10:56:41 +1000

    Add basename for github pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { UpgradesPage } from './pages/UpgradeCostCalc'
 
 const App = () => (
-    <BrowserRouter>
+    <BrowserRouter basename='factory-idle-calculator'>
         <Routes>
             <Route path='/' element={<UpgradesPage />} />
         </Routes>
